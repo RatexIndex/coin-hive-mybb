59a386dbc38e1ed136f17fe81e6a3c3130b05b7c
RatexIndex <webmaster@ratexindex.de>
2017-09-18 22:33:54 +0200

    Update miner.php

--- miner.php
+++ miner.php
@@ -1,11 +1,11 @@
 <?php
 define("IN_MYBB", 1);
-//define("NO_ONLINE", 1); // Wenn Seite nicht in Wer ist online-Liste auftauchen soll
+//define("NO_ONLINE", 1);
  
 require("global.php");
  
 add_breadcrumb("Your Website - Miner", "miner.php");
  
-eval("\$miner = \"".$templates->get("miner")."\";"); // Hier wird das erstellte Template geladen
+eval("\$miner = \"".$templates->get("miner")."\";");
 output_page($miner);
 ?>
